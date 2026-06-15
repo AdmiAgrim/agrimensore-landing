40d745b8c462cc481d129fcd37b73f1000bcb491
Admin <admin@agrimensore.com>
2026-06-15 22:36:52 +0200

    chore(landing): disallow /print in robots

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/_next/', '/actions/'],
+        disallow: ['/api/', '/_next/', '/actions/', '/print/'],
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
